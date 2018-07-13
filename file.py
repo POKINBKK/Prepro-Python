f4c960855bf06c07464920e2210d391103e6c45d
POK <mr.pokiloli@gmail.com>
2018-07-13 11:27:56 +0700

    Add File

---
+++ file.py
@@ -0,0 +1,38 @@
+"""
+Pre-Programming 61 Onsite
+Week 3 - List
+"""
+
+def main():
+    """ Main function """
+    matrix = list()
+    row_amount, col_amount = int(input()), int(input())
+
+    for _ in range(row_amount):
+        matrix.append([])
+        for _ in range(col_amount):
+            matrix[-1].append(int(input()))
+
+    for i in matrix:
+        for j in i:
+            print(j, end=' ')
+        print()
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
